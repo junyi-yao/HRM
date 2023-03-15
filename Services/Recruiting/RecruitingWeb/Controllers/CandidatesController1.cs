30340bdc85a43eac2bd5fc69cb30350c950db297
junyi-yao <93753024+junyi-yao@users.noreply.github.com>
2023-03-15 12:00:54 -0400

    Initial commit

---
+++ Services/Recruiting/RecruitingWeb/Controllers/CandidatesController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitingWeb.Controllers
+{
+    public class CandidatesController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Services/Recruiting/RecruitingWeb/Controllers/JobsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitingWeb.Controllers
+{
+    public class JobsController : Controller
+    {
+        [HttpGet] //attributes in C#
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //Authenticated and User should have role for creating new job
+        //HR/Manager
+        [HttpPost]
+        public IActionResult Create()
+        {
+            //take the information from the view and save to DB
+            return View();
+        }
+    }
+}
