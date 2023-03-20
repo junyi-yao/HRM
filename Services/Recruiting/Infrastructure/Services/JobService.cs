3140b8ad8a28619a1b8aeb1bbf16e0dfe6ab7b26
junyi-yao <93753024+junyi-yao@users.noreply.github.com>
2023-03-20 10:28:55 -0400

    Dependency Injection

---
+++ Services/Recruiting/Infrastructure/Services/JobService.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class JobService:IJobService
+    {
+        public List<JobResponseModel> GetAllJobs() {
+
+            var jobs = new List<JobResponseModel>() {
+                new JobResponseModel{Id = 1, Title = ".NET Developer", Description = "Need to Know SQL and C#"},
+                new JobResponseModel{Id = 2, Title = "Full Stack Developer", Description = "Need to Know C#"},
+                new JobResponseModel{Id = 3, Title = "Java Developer", Description = "Need to Know JAVA"},
+                new JobResponseModel{Id = 4, Title = "JavaScript Developer", Description = "Need to Know Javascript"}
+            };
+            return jobs;
+        }
+
+        public JobResponseModel GetJobById(int id) {
+            return new JobResponseModel { Id = 4, Title = "Javascript Dev", Description = "Need to know JAVA"};
+        }
+    }
+}
--- Services/Recruiting/RecruitingWeb/Controllers/JobsController.cs
+++ Services/Recruiting/RecruitingWeb/Controllers/JobsController.cs
@@ -1,18 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Contracts.Services;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RecruitingWeb.Controllers
 {
     public class JobsController : Controller
     {
+        
+        private IJobService _jobService;
+
+        public JobsController(IJobService jobService)
+        {
+            _jobService = jobService;
+        }
+        
         [HttpGet] //attributes in C#
         public IActionResult Index()
         {
+            //we need to get list of Jobs
+            //call the Job Service
+            //var jobService = new JobService();
+            var jobs = _jobService.GetAllJobs();
             return View();
         }
 
         [HttpGet]
         public IActionResult Details(int id)
         {
+            //var jobService = new JobService();
+            var job = _jobService.GetJobById(id);
             return View();
         }
 
