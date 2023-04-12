4e1104ba2f5fc6fff96ab875836b7847ba95b097
junyi-yao <93753024+junyi-yao@users.noreply.github.com>
2023-04-11 21:24:42 -0400

    commit from laptop

---
+++ Services/Interviews/Interviews.API/Controllers/InterviewsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interviews.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterviewsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAllInterviews()
+        {
+            //go to database and get all the interviews based on role
+            //if role is admin, get all
+            //if role is manager, get only manager's interviews
+            //read the header using HttpContext
+            //JWT token
+            //Authorization Header, Bearer Your_Token
+            //userid, roles
+            //decode the JWT to C# object
+
+            var interviews = new List<String>(new[] {"abc, xyz, ddd","aasda"});
+            return Ok(interviews);
+        }
+    }
+}
--- Services/Interviews/Interviews.API/Program.cs
+++ Services/Interviews/Interviews.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme  ).
+    AddJwtBearer(options => 
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "HRM",  //in AccountController, Issuer = "HRM"
+            ValidAudience = "HRM Users",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
+        };
+    }
+    );
+//Microsoft.AspNetCore.Authentication.JwtBearer
+//Microsoft.IdentityModel.Tokens
 
 var app = builder.Build();
 
@@ -18,7 +39,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
 
 app.MapControllers();
 
